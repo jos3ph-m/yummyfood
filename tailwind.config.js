0295214f09401d64be7f30e189c427b16be654d6
Joseph M <jmedina1985@hotmail.com>
2020-11-07 22:33:54 -0800

    Custom colors and font family added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
